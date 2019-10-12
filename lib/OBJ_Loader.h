6270f9afbb3a3f52bbcae6ad92dc94bc63ec30ca
Мира Странная <miraityan2004@gmail.com>
2019-10-13 01:55:02 +0400

    Minor fixes in OBJ-Loader lib

--- lib/OBJ_Loader.h
+++ lib/OBJ_Loader.h
@@ -174,7 +174,6 @@ namespace objl
     {
         Material()
         {
-            name;
             Ns = 0.0f;
             Ni = 0.0f;
             d = 0.0f;
@@ -976,7 +975,7 @@ namespace objl
 
                     // If Vertex is not an interior vertex
                     float angle = math::AngleBetweenV3(pPrev.Position - pCur.Position, pNext.Position - pCur.Position) * (180 / 3.14159265359);
-                    if (angle <= 0 && angle >= 180)
+                    if (angle <= 0 || angle >= 180)
                         continue;
 
                     // If any vertices are within this triangle
