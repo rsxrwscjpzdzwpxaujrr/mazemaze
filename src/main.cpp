7950709563dc78ad58ec301693ade3da96b34aba
Мира Странная <miraityan2004@gmail.com>
2020-06-04 09:26:11 +0400

    Simplify locale path

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,6 @@
 
 #include "GraphicEngine.hpp"
 #include "Settings.hpp"
-#include "path_separator.hpp"
 
 #include "Gui/MainMenu.hpp"
 
@@ -30,7 +29,7 @@ using namespace mazemaze;
 
 int
 main() {
-    bindtextdomain("mazemaze", "." PATH_SEPARATOR "locale");
+    bindtextdomain("mazemaze", "locale");
     textdomain("mazemaze");
     bind_textdomain_codeset("mazemaze", "UTF-8");
 
